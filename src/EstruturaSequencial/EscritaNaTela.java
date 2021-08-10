4f5499a38e684a8bb33e7a91420d1ac407be0d1b
raquelalvesas <raquelalvesas@gmail.com>
2021-08-09 23:42:25 -0300

    first commit

---
+++ src/EstruturaSequencial/EscritaNaTela.java
@@ -0,0 +1,11 @@
+package EstruturaSequencial;
+
+public class EscritaNaTela {
+
+    public static void main(String[] args) {
+
+        System.out.println("Bom dia!");
+
+        System.out.print("Bom dia!");
+    }
+}
