5e51b028a37f8e92b7f1a6eea81c05195619ee1d
raquelalvesas <raquelalvesas@gmail.com>
2021-08-11 17:57:45 -0300

    bank

---
+++ src/bank/Account.java
@@ -0,0 +1,50 @@
+package bank;
+
+import javax.swing.*;
+
+public class Account {
+
+    public static void main(String[] args) {
+        // Fazer um programa que realize o cadastro de uma conta, dando a opção de depósito.
+        // Em seguida, realizar um depósito e depois um saque, sempre mostrando os dados da conta após cada operação.
+    }
+
+    private int number;
+    private Spring holder;
+    private double balance;
+
+    public Account(int number, Spring holder) {
+        this.number = number;
+        this.holder = holder;
+    }
+
+    public Account(int number, Spring holder, double initialDeposit) {
+        this.number = number;
+        this.holder = holder;
+        deposit(initialDeposit);
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Spring getHolder() {
+        return holder;
+    }
+
+    public void setHolder(Spring holder) {
+        this.holder = holder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount + 5;
+    }
+}
