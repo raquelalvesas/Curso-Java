528781a39b92b4c2bd55f98c62ab32d43ff2d9b5
raquelalvesas <raquelalvesas@gmail.com>
2021-08-12 12:57:32 -0300

    vetor

---
+++ src/vetor/Exercicio.java
@@ -0,0 +1,34 @@
+package vetor;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio {
+
+    public static void main(String[] args) {
+
+        // Fazer um programa para ler um número inteiro N e a altura de N pessoas.
+        // Armazene as N alturas em um vetor. Em seguida, mostrar a altura média dessas pessoas.
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        double[] vect = new double[n];
+
+        for (int i=0; i<n; i++) {
+            vect[i] = sc.nextDouble();
+        }
+
+        double sum = 0;
+        for (int i=0; i<n; i++) {
+            sum += vect[i];
+        }
+
+        double avg = sum / n;
+
+        System.out.printf("Average height: %.2f%n", + avg);
+
+        sc.close();
+    }
+}
