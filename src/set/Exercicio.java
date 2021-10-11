b41275ceb4dd4a664cbaa473809628509bcf7a7d
raquelalvesas <raquelalvesas@gmail.com>
2021-10-11 15:26:22 -0300

    set

--- src/set/Exercicio.java
+++ src/set/Exercicio.java
@@ -1,5 +1,10 @@
 package set;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Exercicio {
 
     public static void main(String[] args) {
@@ -8,5 +13,17 @@ public class Exercicio {
         //(apenas uma palavra) e o instante em que o usuário acessou o site no padrão ISO 8601, separados por espaço,
         //conforme exemplo. Fazer um programa que leia o log de acessos a partir de um arquivo, e daí informe quantos
         //usuários distintos acessaram o site.
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter file full path: ");
+        String path = sc.nextLine();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        sc.close();
     }
 }
--- src/set/LogEntry.java
+++ src/set/LogEntry.java
@@ -1,4 +1,48 @@
 package set;
 
+import java.util.Date;
+import java.util.Objects;
+
 public class LogEntry {
+
+    public static void main(String[] args) {
+
+    }
+
+    private String username;
+    private Date moment;
+
+    public LogEntry(String username, Date moment) {
+        this.username = username;
+        this.moment = moment;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof LogEntry)) return false;
+        LogEntry logEntry = (LogEntry) o;
+        return getUsername().equals(logEntry.getUsername());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUsername());
+    }
 }
