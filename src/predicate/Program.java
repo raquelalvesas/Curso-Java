666fa9bf28e168064f8412e866411b71b781e94f
raquelalvesas <raquelalvesas@gmail.com>
2021-10-13 15:31:10 -0300

    predicate

---
+++ src/predicate/Program.java
@@ -0,0 +1,10 @@
+package predicate;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        // Fazer um programa que, a partir de uma lista de produtos, remova da
+        // lista somente aqueles cujo preço mínimo seja 100.
+    }
+}
