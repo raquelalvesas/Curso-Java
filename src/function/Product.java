6047e4ded3d90789238d9ef016f00c0e6fd5ef8f
raquelalvesas <raquelalvesas@gmail.com>
2021-10-14 15:08:56 -0300

    function

---
+++ src/function/Product.java
@@ -0,0 +1,16 @@
+package function;
+
+public class Product {
+
+    public static void main(String[] args) {
+
+    }
+
+    private String name;
+    private Double price;
+
+    public Product(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+}
---
+++ src/function/Program.java
@@ -0,0 +1,25 @@
+package function;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        //Fazer um programa que, a partir de uma lista de produtos, gere uma
+        //nova lista contendo os nomes dos produtos em caixa alta.
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+        list.add(new Product("Tv", 900.00));
+
+
+
+
+
+    }
+}
